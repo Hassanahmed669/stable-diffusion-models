329a45996f3e5e71d32de8cc79fd091c0b16c90b
root <root@ip-172-31-4-4.ap-south-1.compute.internal>
2023-11-22 18:32:43 +0000

    update s3

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,7 @@
 export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
 export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
 export AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION
+export S3_PATH=$S3_PATH
 
 aws s3 cp $S3_PATH /app/models/Stable-diffusion/
 
