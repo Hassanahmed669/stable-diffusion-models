c0ad9a3a5d047e594c03cf1e6d283a5ec1bead13
root <root@ip-172-31-35-190.ap-south-1.compute.internal>
2023-12-01 22:42:30 +0000

    update the APP_VERSION and tmpfs

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+mount -t tmpfs -o rw,size=2048m tmpfs /tmp
+
 # Set AWS credentials
 export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
 export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
 export AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION
 export S3_PATH=$S3_PATH
+export APP_VERSION=$APP_VERSION
 
 aws s3 cp $S3_PATH /app/models/Stable-diffusion/
 
